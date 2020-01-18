fbe9357ac9b368c14832fc97615ad35c717a3b6f
Min Yu <appleboy1024@yahoo.com.tw>
2020-01-18 13:50:31 +0800

    THIS IS A SECOND COMMIT

---
+++ HW1_1_1.java
@@ -0,0 +1,15 @@
+package ch3;
+
+public class HW1_1 {
+
+	public static void main(String[] args) {
+		int a = 12 ,b = 6;
+		int sum = a + b;
+		int pro = a * b;
+		String s1="和 =";
+		String s2="積 =";
+		System.out.println(s1+sum);
+		System.out.println(s2+pro);
+	}
+
+}
